886ae5bb1e6dd7957ddab6fa3d846d3a0ee6b42c
aksoyece <aksoyece241@gmail.com>
2026-09-02 22:05:30 +0300

    Nuxt 4 proje iskeletini kurup bağımlılıkları ekledim

---
+++ nuxt.config.ts
@@ -0,0 +1,36 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxtjs/supabase'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  runtimeConfig: {
+    public: {
+      appName: 'Apartman Yönetim Sistemi'
+    }
+  },
+
+  supabase: {
+    redirect: false,
+    types: false
+  },
+
+  compatibilityDate: '2026-06-30',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
